1f23252d4bf472cf04dd878c3103d984a143c7ab
Gfrim <tophcker4@gmail.com>
2022-09-20 22:48:03 +0000

    function that computes a to the power of b

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 def pow(a, b):
-    return pow(a, b)
+    result = a
+    for i in range(b):
+        if (b != 2):
+            result *= a
+        else:
+            result = a * a
+    return result
