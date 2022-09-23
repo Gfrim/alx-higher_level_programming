cd60f374c32e11bd3de41fc8b58d693f1aca1595
Gfrim <tophcker4@gmail.com>
2022-09-23 15:01:33 +0000

    Easy print

--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,7 +1,2 @@
 #!/usr/bin/python3
-import sys
-output = sys.stdout
-text = "#pythoniscool"
-for i in text:
-    output.write(i)
-output.write("\n")
+__import__('sys').stdout.write("#pythoniscool\n")
