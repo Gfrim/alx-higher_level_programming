130e23e2aea0f14c8ff2bac617687cd336a01197
Gfrim <tophcker4@gmail.com>
2022-10-11 13:23:47 +0000

    Square class definition with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# 1-sqaure.py by GF
+"""Defines a square class with
+   initialization of an attribute
+"""
+
+class Square:
+    """ Initialize size attribute"""
+    __size = 0
+    pass
