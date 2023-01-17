0bc170c43ddc204d026658969668c682aa7d10ff
Gfrim <gfrim79@gmail.com>
2023-01-17 06:48:06 -0800

    curl post parameters

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # a script that takes a url and sends a post to the passed url
-curl -s -x POST -d "email=test@gmail.com.com&subject=Python Networks" "$1"
+curl -s -x POST -d "email=test@gmail.com.com&subject=I will always be here for you" "$1"
